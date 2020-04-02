033f3963e514592aeb5e7451db5ec50f162b5551
Wittaya-Nest <wittaya.nest@gmail.com>
2020-04-03 03:01:17 +0700

    fix-flash

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('./config/strategies/express')
 
-
-// initalization
 const app = express()
-//Server is Listening
+
 app.listen(app.get('port'), function(){
     console.log('Server on port', app.get('port'))
 })
-module.exports.app;
+
+module.exports = app
 
 
 
